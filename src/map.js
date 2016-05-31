b219143c8ba7d63b4d78439f4c968762c4d83969
Sinatra <root@stackin.money>
2016-05-31 00:12:50 -0400

    Create map.js

---
+++ src/map.js
@@ -0,0 +1,4 @@
+<div class="sidebar"> <a> JoyTracker</a> <br></br><label for="user" style="font-size: 21px;"> Identity </label><input type="text" id="user" name="username"></input>
+</div>
+<div id="map"></div>
+<script src="https://maps.googleapis.com/maps/api/js?key=LOL&callback=initMap" async defer></script>
