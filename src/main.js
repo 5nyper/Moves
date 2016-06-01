bbde60b3f2206f147e851148f70673ac8ef57be0
Sinatra <root@stackin.money>
2016-05-31 21:12:32 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var moment = require('moment');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -11,6 +12,14 @@ io.on('connection', function (socket) {
     console.log("got spot, sending mark");
     socket.emit('mark', data);
     socket.broadcast.emit('mark', data);
+    var interv = setInterval(function() {
+      console.log(parseInt(moment().format('hmmss')) - data.time);
+      if (parseInt(moment().format('hmmss')) - data.time > 120) {
+        socket.emit('delete');
+        socket.broadcast.emit('delete');
+        clearInterval(interv);
+      }
+    }, 3000);
   });
   socket.on('disconnect', function () {
     console.log("User disconnected");
