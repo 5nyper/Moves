1610d6ce37c5aaf1a59491ca791e81a5c3367119
Sinatra <root@stackin.money>
2016-05-31 20:10:27 -0400

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function (socket) {
+  console.log("User connected");
+  socket.on('spot', function(data) {
+    console.log("got spot, sending mark");
+    socket.emit('mark', data);
+    socket.broadcast.emit('mark', data);
+  });
+  socket.on('disconnect', function () {
+    console.log("User disconnected");
+  });
+});
+
+server.listen(3000);
